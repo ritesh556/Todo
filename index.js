84df99145933d8083e634a93f9cff3cb6eeba17f
Ritesh Nepal <58138580+ritesh556@users.noreply.github.com>
2023-02-22 16:03:05 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
 let text;
-var a = [];
+let a = [];
 
-document.getElementById("ok").onclick = function(){
-    let i;
-    text = document.getElementById("text").value;
-   if (text.length === 0 || text.length >= 100){
-    
-        var check = document.getElementById("check");
-        if (check.innerHTML !== "enter what to do") {
-            check.innerHTML += "enter what to do";
-        }
+// Function to render the list of items
+function renderList() {
+    document.getElementById("write").innerHTML = "";
+    for (let i = 0; i < a.length; i++) {
+        document.getElementById("write").innerHTML += (i + 1) + ")" + (a[i]) + "&emsp;" + "<button class='delete' data-index='" + i + "'>Delete</button>&emsp;<button class='edit' data-index='" + i + "'>Edit</button><br>";
     }
-    
-   
-   else{
 
-    a.push(text);
-    localStorage.setItem("a", JSON.stringify(a));
-    
-    document.getElementById("write").innerHTML ="";
-    for (i=0;i<a.length;i++){
-        document.getElementById("write").innerHTML += (i+1) + ")" + (a[i])  + "&emsp;" + "<button class='delete' data-index='"+i+"'>Delete</button><br>";
-    }
-    document.getElementById("text").value = "";
-    document.getElementById("check").innerHTML =""
-}
-}
-
-
-window.onload = function() {
-    a = JSON.parse(localStorage.getItem("a")) || []; 
-    for (i=0;i<a.length;i++){
-        document.getElementById("write").innerHTML += (i+1) + ")"  + (a[i])  + "&emsp;" + "<button class='delete' data-index='"+i+"'>Delete</button><br>";
-    }
+    // Add event listeners to delete and edit buttons
     document.querySelectorAll(".delete").forEach(function(button) {
         button.addEventListener("click", function() {
             let index = button.getAttribute("data-index");
             a.splice(index, 1);
             localStorage.setItem("a", JSON.stringify(a));
-            
-document.getElementById("write").innerHTML = "";
-for (i=0;i<a.length;i++){
-    document.getElementById("write").innerHTML += (i+1) + ")"  + (a[i])  + "&emsp;" + "<button class='delete' data-index='"+i+"'>Delete</button><br>";
+            renderList();
+        });
+    });
+
+    document.querySelectorAll(".edit").forEach(function(button) {
+        button.addEventListener("click", function() {
+            let index = button.getAttribute("data-index");
+            let newText = prompt("Enter new text", a[index]);
+            if (newText && newText.length > 0 && newText.length < 100) {
+                a[index] = newText;
+                localStorage.setItem("a", JSON.stringify(a));
+                renderList();
+            }
+        });
+    });
 }
+
+// Event listener for "Add" button
+document.getElementById("ok").addEventListener("click", function(event) {
+    event.preventDefault();
+    text = document.getElementById("text").value;
+    if (text.length === 0 || text.length >= 100) {
+        var check = document.getElementById("check");
+        if (check.innerHTML !== "enter what to do") {
+            check.innerHTML += "enter what to do";
+        }
+    } else {
+        a.push(text);
+        localStorage.setItem("a", JSON.stringify(a));
+        document.getElementById("text").value = "";
+        document.getElementById("check").innerHTML = "";
+        renderList();
+    }
 });
-})};
+
+// Load the list of items from local storage when the page loads
+window.onload = function() {
+    a = JSON.parse(localStorage.getItem("a")) || [];
+    renderList();
+};
